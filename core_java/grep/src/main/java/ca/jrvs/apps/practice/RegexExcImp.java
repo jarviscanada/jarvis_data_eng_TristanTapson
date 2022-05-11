2c031aface62d1913ba7a387cf80c890ea53739e
Tristan Tapson <tapsonte@gmail.com>
2022-05-11 18:24:17 +0000

    regex practice completed

--- core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExcImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExcImp.java
@@ -1,27 +1,50 @@
 package ca.jrvs.apps.practice;
+import java.util.regex.Pattern;
 
 public class RegexExcImp implements RegexExc {
 
+	// .jpeg | .jpg file
 	public boolean matchJpeg(String filename){
 
+		if(Pattern.matches("^.+(.jpg|.jpeg)$", filename.toLowerCase())){
+			return true;
+		}
+
 		return false;
 	}
 
+	// IPv4 address
 	public boolean matchIp(String ip){
 
+		if(Pattern.matches(   "^([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3})$", ip)){
+			return true;
+		}
+
 		return false;
 	}
 
+	// empty line
 	public boolean isEmptyLine(String line){
+
+		if(Pattern.matches("^(\\s)*" , line)){
+			return true;
+		}
 
 		return false;
 	}
 
 	public static void main(String args[]) {
 
-		System.out.println("RegexExcImp Test");
+		System.out.println("RegexExcImp class!");
 
-		RegexExcImp test = new RegexExcImp();
-		System.out.println(test.matchJpeg("filename"));
+		// instance of class created
+		RegexExcImp testRE = new RegexExcImp();
+
+		// testing
+		/*
+		System.out.println(testRE.matchJpeg("xyz.JpEg"));
+		System.out.println(testRE.matchIp("222.34.3.009"));
+		System.out.println(testRE.isEmptyLine("	"));
+		*/
 	}
 }
