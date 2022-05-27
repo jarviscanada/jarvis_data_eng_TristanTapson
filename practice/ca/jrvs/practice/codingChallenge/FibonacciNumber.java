2877ec31a04cfea4cbe3980bd7ba20daf636a3e0
Tristan Tapson <tapsonte@gmail.com>
2022-05-27 23:27:04 +0000

    comment fix

--- practice/ca/jrvs/practice/codingChallenge/FibonacciNumber.java
+++ practice/ca/jrvs/practice/codingChallenge/FibonacciNumber.java
@@ -9,9 +9,9 @@ import java.util.Arrays;
  *              called the Fibonacci sequence, such that each number is the sum
  *              of the two preceding ones, starting from 0 and 1. That is:
  *
- * F(0) = 0, F(1) = 1
- * F(n) = F(n - 1) + F(n - 2), for n > 1.
- * Given n, calculate F(n).
+ *              F(0) = 0, F(1) = 1
+ *              F(n) = F(n - 1) + F(n - 2), for n > 1.
+ *              Given n, calculate F(n).
  *
  * NOTE: recursive, iterative, and dynamic approach are implemented
  */
