e8a8b17fe003f460ea6efcef3b09fdb869dfc536
Tristan Tapson <tapsonte@gmail.com>
2022-06-05 03:02:50 +0000

    dao implementation start

--- core_java/twitter/ca/jrvs/apps/twitter/dao/helper/HttpHelper.java
+++ core_java/twitter/ca/jrvs/apps/twitter/dao/helper/HttpHelper.java
@@ -1,23 +1,28 @@
 package ca.jrvs.apps.twitter.dao.helper;
 
 
+import java.io.IOException;
 import java.net.URI;
+
+import oauth.signpost.exception.OAuthCommunicationException;
+import oauth.signpost.exception.OAuthExpectationFailedException;
+import oauth.signpost.exception.OAuthMessageSignerException;
 import org.apache.http.HttpResponse;
 import org.apache.http.entity.StringEntity;
 
 public interface HttpHelper {
 
     /**
-     * Execute a HTTP Post call
+     * Execute a HTTP Post call (post tweet to account by status text)
      * @param uri
      * @return
      */
-    HttpResponse httpPost(URI uri);
+    HttpResponse httpPost(URI uri) throws OAuthMessageSignerException, OAuthExpectationFailedException, OAuthCommunicationException, IOException;
 
     /**
-     * Execute a HTTP Get call
+     * Execute a HTTP Get call (get tweet info by id)
      * @param uri
      * @return
      */
-    HttpResponse httpGet(URI uri);
+    HttpResponse httpGet(URI uri) throws OAuthMessageSignerException, OAuthExpectationFailedException, OAuthCommunicationException, IOException;
 }
---
+++ core_java/twitter/ca/jrvs/apps/twitter/dao/helper/TwitterHttpHelper.java
@@ -0,0 +1,67 @@
+package ca.jrvs.apps.twitter.dao.helper;
+
+import oauth.signpost.OAuthConsumer;
+import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
+import oauth.signpost.exception.OAuthCommunicationException;
+import oauth.signpost.exception.OAuthExpectationFailedException;
+import oauth.signpost.exception.OAuthMessageSignerException;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.net.URI;
+
+/** TODO: make the get/post request modular b/c they are similar
+ *          also include a try/catch in the http get/post functions after modularizing
+ */
+public class TwitterHttpHelper implements HttpHelper{
+
+    /**
+     * Dependencies are specified as private member variables
+     */
+    private OAuthConsumer consumer;
+    private HttpClient httpClient;
+
+    /**
+     * Constructor
+     * Setup dependencies using secrets
+     *
+     * @param consumerKey
+     * @param consumerSecret
+     * @param accessToken
+     * @param tokenSecret
+     */
+    public TwitterHttpHelper(String consumerKey, String consumerSecret,
+        String accessToken, String tokenSecret){
+        consumer = new CommonsHttpOAuthConsumer(consumerKey, consumerSecret);
+        consumer.setTokenWithSecret(accessToken, tokenSecret);
+
+        // default single connection
+        httpClient = new DefaultHttpClient();
+    }
+
+    @Override
+    public HttpResponse httpPost(URI uri) throws OAuthMessageSignerException, OAuthExpectationFailedException, OAuthCommunicationException, IOException {
+
+        HttpPost postRequest = new HttpPost(uri);
+        consumer.sign(postRequest);
+        HttpResponse response = httpClient.execute(postRequest);
+        System.out.println(EntityUtils.toString(response.getEntity()));
+        return response;
+
+    }
+
+    @Override
+    public HttpResponse httpGet(URI uri) throws OAuthMessageSignerException, OAuthExpectationFailedException, OAuthCommunicationException, IOException {
+
+        HttpGet getRequest = new HttpGet(uri);
+        consumer.sign(getRequest);
+        HttpResponse response = httpClient.execute(getRequest);
+        System.out.println(EntityUtils.toString(response.getEntity()));
+        return response;
+    }
+}
